164b18a1126cecd74768fc05610bdedf7ff770d7
Furkan Şahin <a.furkansahinnn@gmail.com>
2025-04-03 06:28:34 +0300

    Mobile app dosyaları eklendi

---
+++ mobile-app/App.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import "./global.css";
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
+      <Text className="text-black">Open up App.js to start working on your app!</Text>
+      <Text className="text-black">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
