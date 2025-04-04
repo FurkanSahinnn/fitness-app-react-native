79fed0d1b4690f04d58fe171c7130a8b5f8c8b81
Furkan Şahin <a.furkansahinnn@gmail.com>
2025-04-04 03:58:17 +0300

    User service ve controller eklendi.

---
+++ api-server/src/controllers/userController.js
@@ -0,0 +1,72 @@
+import { createUserService, getAllUsersService, getUserByIdService, updateUserService, deleteUserService } from "../services/userService.js";
+
+const handleResponse = (res, status, message, data = null) => {
+    res.status(status).json({
+        message,
+        data
+    });
+}
+
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await getAllUsersService();
+        handleResponse(res, 200, "Users fetched successfully", users);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getUserById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await getUserByIdService(id);
+        if (!user) {
+            handleResponse(res, 404, "User not found");
+            return;
+        }
+        handleResponse(res, 200, "User fetched successfully", user);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const createUser = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await createUserService(name, email, password);
+        handleResponse(res, 201, "User created successfully", user);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name, email } = req.body;
+        const user = await updateUserService(id, name, email);
+        if (!user) {
+            handleResponse(res, 404, "User not found");
+            return;
+        }
+        handleResponse(res, 200, "User updated successfully", user);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await deleteUserService(id);
+        if (!user) {
+            handleResponse(res, 404, "User not found");
+            return;
+        }
+        handleResponse(res, 200, "User deleted successfully", user);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
---
+++ api-server/src/services/userService.js
@@ -0,0 +1,34 @@
+import pool from "../config/dbConfig.js";
+
+export const getAllUsersService = async () => {
+    const result = await pool.query("SELECT * FROM users");
+    return result.rows;
+}
+
+export const getUserByIdService = async (id) => {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", 
+        [id]
+    );
+    return result.rows[0];
+}
+
+export const createUserService = async (name, email, password) => {
+    const result = await pool.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *", 
+        [name, email, password]
+    );
+    return result.rows[0];
+}
+
+export const updateUserService = async (id, name, email) => {
+    const result = await pool.query("UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *", 
+        [name, email, id]
+    );
+    return result.rows[0];
+}
+
+export const deleteUserService = async (id) => {
+    const result = await pool.query("DELETE FROM users WHERE id = $1", 
+        [id]
+    );
+    return result.rows[0];
+}
